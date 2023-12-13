b1468f8a883cd1749ecb4d38fa8dc87942730b0e
cReih <122928197+creih@users.noreply.github.com>
2023-12-13 22:24:38 +0300

    removed the environ

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[])
 {
 	int result = argc;
-	char *input = NULL, *command, **ptr_env = environ;
+	char *input = NULL, *command;
 	size_t len = 0, izasomwe, x = -1;
 
 	while (1)
@@ -23,15 +23,6 @@ int main(int argc, char *argv[])
 		{
 			free(input);
 			exit(EXIT_FAILURE);
-		}
-		if (is_equal(command, "env"))
-		{
-			while (*ptr_env != NULL)
-			{
-				write(STDOUT_FILENO, *ptr_env, sizeof(*ptr_env));
-				write(STDOUT_FILENO, "\n", 1);
-				ptr_env++;
-			}
 		}
 		exec_cmd(command, argv[0]);
 	}
