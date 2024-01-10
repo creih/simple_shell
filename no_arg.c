605e38b1377040ac451bc362e927514b05d01c43
creih <iherr.9@gmail.com>
2024-01-11 00:11:01 +0200

    retrying the shell gate

---
+++ no_arg.c
@@ -0,0 +1,41 @@
+#include "string.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	char *input, **args;
+	size_t len = 0;
+	pid_t pid;
+
+	while(1)
+	{
+		printf("#cisfun$ ");
+		getline(&input, &len, stdin);
+		input[strlen(input) - 1] = '\0';
+		pid = fork();
+		if(pid == -1)
+		{
+			perror("Fork failed");
+			exit(EXIT_FAILURE);
+		}
+		else if(pid == 0)
+		{
+			*args[] = {input, NULL};
+			if (execve(input, args, NULL) == -1)
+			{
+				perror("Execution failed");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	free(input);
+	return (0);
+}
--- shell.h
+++ shell.h
@@ -9,16 +9,5 @@
 #include <sys/wait.h>
 #include <errno.h>
 #define MAX_INPUT_SIZE 1024
-extern char **environ;
-
-void print_prompt(void);
-int execute_command(char *command, char *arg_1);
-bool is_equal(const char *str1, const char *str2);
-void trim_new_line(char *input);
-void exec_cmd(char *command, char *shell_name);
-size_t _strlen(char *s);
-void n_interactive(int bara, char *zarg[]);
-void ese_ni_interactive(int bara, char *zarg[]);
-void exec_env(void);
 
 #endif
